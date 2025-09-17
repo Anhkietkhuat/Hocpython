3b8fb8b2e4e8d6d385128a283b124eedc6c30bfe
Anhkietkhuat <kiet87902@gmail.com>
2025-09-17 17:09:33 +0700

    up

---
+++ Hoc_Python/VD1.py
@@ -0,0 +1,16 @@
+
+# Biến
+a = 5
+b = 100
+x = 10
+y = 8.9
+name = "Anh Kiet"
+
+#kiểm tra kiểu biến
+
+print(type(a))
+print(type(y))
+print(type(name))
+
+
+
---
+++ Hoc_Python/VD2.py
@@ -0,0 +1,18 @@
+
+
+
+
+#Chuỗi
+a = "Hello"
+c = "World"
+b = 'Python'
+s = """Chuỗi\n nhiều\n dòng"""
+
+# in ra
+print(a)
+print(c)
+print(s)
+print(a + "" + c) #nối chuỗi
+
+# lặp trong chuỗi
+
