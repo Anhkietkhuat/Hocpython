18704768b333c37012247090d3944b6143227d6a
Anhkietkhuat <kiet87902@gmail.com>
2025-09-24 17:36:59 +0700

    update_python

---
+++ Hoc_Python/VD_Set.py
@@ -0,0 +1,64 @@
+
+
+# set_1={69,96}
+# print(set_1)
+#
+# set_2 = set()
+# print(type(set_2))
+#
+# set_3=set([1,2,3,3,6,7,8,1,1,1,3,4])
+# print(set_3)
+# print(type(set_3))
+#
+#kiểm tra phần tử nằm trong set
+print(1 in {1,2,3,4})
+print(5 in {1,2,3,4})
+
+print({1,2,3,4}-{3,4})
+print({1,2}-{1,2})
+
+# #toán tử &(giao)
+# print({1,3,2}&{3,4,5})
+
+# #toán tử | : gộp tất cả phần tử ,bỏ phần tử trùng
+# print({1,3,2}|{4,5})
+# print({1,3,2}|{1,2,3,4,5})
+
+# toán tử ^
+# print({1,3,2}^{3,4,5})
+
+# set1 = {1,2,3}
+# set2 = {3,4,5,6}
+# set3 = set1 & set2
+# set4 = set1 | set2
+# set5 = set4 - set3
+#
+# print(set3)
+# print(set4)
+# set5.clear()
+# print(set5)
+
+# # phương thức pop: xóa và trả về phần tử bất kì
+# set5 = {3,4,5,6}
+# set5.pop()
+# print(set5)
+
+# #phương thức remove
+# set1 = {1,3,4,5,2}
+# set1.remove(3)
+# print(set1)
+#
+
+#
+#phương thức copy
+# set1 = {1,3,4,5,2}
+# set2 = set1.copy()
+# print(set1)
+# print(set2)
+
+
+# #phương thức add
+# set1 = {1,3,4,5,2}
+# set1.add(8)
+# print(set1)
+#
